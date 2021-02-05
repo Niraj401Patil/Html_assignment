2ee8a19cf675d45139c2ed88b2ae94f6a534501b
Niraj patil <niraj40patil@gmail.com>
2021-02-05 21:45:34 +0530

    hhh

---
+++ src/App.jsx
@@ -0,0 +1,38 @@
+import React from 'react'
+
+function App(props) 
+{
+
+let curDate =new Date();
+curDate=curDate.getHours();
+
+let msg='';
+
+if (curDate>=1 && curDate<=12)
+{
+ msg="Good Morning";
+
+}else if( curDate >=12 && curDate<19)
+ {
+     msg="Good afternoon";
+}else
+ {
+     msg="Good night";
+ }
+
+
+ return (
+
+ <> 
+ <h1 style={{ fontFamily: 'Noto Sans JP', fontWeight: 'bold', color:'pink'}}>
+  {`Hello ${msg}  My name is - ${props.name} and i am a ${props.age} year old`} </h1>
+ 
+ 
+ </> );
+
+}
+
+
+
+export default App;
+
---
+++ src/index.js
@@ -0,0 +1,24 @@
+import React from 'react'
+import ReactDom from 'react-dom'
+
+import App from './App'
+//import Calc_app from './Calc_app'
+
+
+
+
+ReactDom.render( 
+<>
+  
+  <App name= "Niraj" age="24"/>
+    
+  
+
+</>,
+  
+  document.getElementById("root")
+  
+);
+
+
+
